38b2be3c69bf0035371da9cadcaff81c265541a0
Liam <liamjwatson@aol.com>
2023-08-12 11:33:27 +0100

    initial commit and folder structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`
+
+
+function App() {
+  return (
+    <div>
+      <H1>Hello World</H1>
+    </div>
+  );
+}
+
+export default App;
