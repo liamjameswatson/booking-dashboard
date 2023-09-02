5da64d2d8509db8d55a322ec9c783b5f314dc5aa
Liam <liamjwatson@aol.com>
2023-09-02 17:23:03 +0100

    logout function connected to logout button

--- src/features/authentication/Logout.jsx
+++ src/features/authentication/Logout.jsx
@@ -1,10 +1,15 @@
 import { HiArrowRightOnRectangle } from "react-icons/hi2";
 import ButtonIcon from "../../ui/ButtonIcon";
+import SpinnerMini from "../../ui/SpinnerMini";
+
+import { useLogout } from "./useLogout";
 
 function Logout() {
+  const { logout, isLoading } = useLogout();
+
   return (
-    <ButtonIcon>
-      <HiArrowRightOnRectangle />
+    <ButtonIcon disabled={isLoading} onClick={logout}>
+      {!isLoading ? <HiArrowRightOnRectangle /> : <SpinnerMini />}
     </ButtonIcon>
   );
 }
