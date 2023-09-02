9c4b52adecf98a53c7393a5353a153b92efbca77
Liam <liamjwatson@aol.com>
2023-09-02 13:01:34 +0100

    get current user function added

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -10,3 +10,21 @@ export async function login({ email, password }) {
   console.log(data);
   return data;
 }
+
+
+export async function getCurrentUser() {
+  // gets the session from local storage
+  const {data: session} = await supabase.auth.getSession()
+
+  // if no current user 
+  if(!session.session) return null;
+
+const {data, error} = await supabase.auth.getUser()
+
+console.log(data)
+
+if (error) throw new Error(error.message);
+console.log(data);
+return data?.user;
+
+}
