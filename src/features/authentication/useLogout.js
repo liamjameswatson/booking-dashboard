05dd2e4a7293ff1dd68dad24ab173610652e968e
Liam <liamjwatson@aol.com>
2023-09-02 17:15:53 +0100

    useLogout hook added that calls the logout function, using react query

---
+++ src/features/authentication/useLogout.js
@@ -0,0 +1,16 @@
+import useNavigate from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { logout as logoutApi } from "../../services/apiAuth";
+
+export function useLogout() {
+  const navigate = useNavigate();
+
+  const { mutate: logout, isLoading } = useMutation({
+    mutationFn: logoutApi,
+    onSuccess: () => {
+      navigate("/login", { replace: true });
+    },
+  });
+
+  return { logout, isLoading };
+}
