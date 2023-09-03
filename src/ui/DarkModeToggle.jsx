9cd03c635e4c1257e17cff7da69c62aab4179c30
Liam <liamjwatson@aol.com>
2023-09-03 19:06:14 +0100

    added toggle to button icon,   sun/moon

--- src/ui/DarkModeToggle.jsx
+++ src/ui/DarkModeToggle.jsx
@@ -1,11 +1,17 @@
-import { HiOutlineMoon } from "react-icons/hi2";
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi2";
 import ButtonIcon from "./ButtonIcon";
-import {useDarkMode} from '../context/DarkModeContext'
+import { useDarkMode } from "../context/DarkModeContext";
 
 function DarkModeToggle() {
-  const {isDarkMode, toggleDarkMode} = useDarkMode()
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
 
-  return <ButtonIcon><HiOutlineMoon /></ButtonIcon>;
+  return (
+    <ButtonIcon onClick={toggleDarkMode}>
+      {/* Change icon from moon to sun */}
+      {isDarkMode ? <HiOutlineSun /> : 
+      <HiOutlineMoon />}
+    </ButtonIcon>
+  );
 }
 
 export default DarkModeToggle;
