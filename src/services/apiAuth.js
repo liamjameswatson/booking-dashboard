65a0ce33663e75b1898704910512ac5de39d5607
Liam <liamjwatson@aol.com>
2023-09-03 12:34:42 +0100

    signup function added to apiAuth, using supabase signUp

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,20 @@
 import supabase from "./supabase";
 
+export async function signup({ fullName, email, password }) {
+  const { data, error } = await supabase.auth.signUP({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: "",
+      },
+    },
+  });
+  if (error) throw new Error(error.message);
+  return data;
+}
+
 export async function login({ email, password }) {
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
@@ -11,25 +26,21 @@ export async function login({ email, password }) {
   return data;
 }
 
-
 export async function getCurrentUser() {
   // gets the session from local storage
-  const {data: session} = await supabase.auth.getSession()
+  const { data: session } = await supabase.auth.getSession();
 
-  // if no current user 
-  if(!session.session) return null;
+  // if no current user
+  if (!session.session) return null;
 
-const {data, error} = await supabase.auth.getUser()
+  const { data, error } = await supabase.auth.getUser();
 
+  if (error) throw new Error(error.message);
 
-if (error) throw new Error(error.message);
-
-return data?.user;
-
+  return data?.user;
 }
 
-
 export async function logout() {
-  const {error}  = await supabase.auth.signOut()
+  const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
 }
