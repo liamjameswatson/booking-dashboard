98266882f8ce67916538f5ece2b885f62f2c572b
Liam <liamjwatson@aol.com>
2023-09-04 16:34:07 +0100

    dark mode added to chart

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 import DashboardBox from "./DashboardBox";
 
-
-
+import { useDarkMode } from "../../context/DarkModeContext";
 
 import Heading from "../../ui/Heading";
 import {
@@ -57,26 +56,51 @@ const fakeData = [
   { label: "Feb 06", totalSales: 1450, extrasSales: 400 },
 ];
 
-
+const isDarkMode = true;
 
 function SalesChart() {
+  const { isDarkMode } = useDarkMode();
 
+  const colors = isDarkMode
+    ? {
+        totalSales: { stroke: "#4f46e5", fill: "#4f46e5" },
+        extrasSales: { stroke: "#22c55e", fill: "#22c55e" },
+        text: "#e5e7eb",
+        background: "#18212f",
+      }
+    : {
+        totalSales: { stroke: "#4f46e5", fill: "#c7d2fe" },
+        extrasSales: { stroke: "#16a34a", fill: "#dcfce7" },
+        text: "#374151",
+        background: "#fff",
+      };
 
   return (
     <StyledSalesChart>
       <Heading as="h2">Sales</Heading>
       <ResponsiveContainer height={300} width="100%">
         <AreaChart data={fakeData}>
-          <XAxis dataKey="label" />
-          <YAxis unit="$" />
+          <XAxis
+            dataKey="label"
+            tick={{ fill: colors.text }}
+            tickLine={{ stroke: colors.text }}
+          />
+          <YAxis
+            unit="$"
+            tick={{ fill: colors.text }}
+            tickLine={{ stroke: colors.text }}
+          />
 
           <CartesianGrid strokeDasharray="4" />
-          <Tooltip />
+          <Tooltip contentStyle={{ backgroundColor: colors.background }} />
           <Area
             dataKey="totalSales"
             type="monotone"
-            stroke="red"
-            fill="orange"
+            stroke={colors.totalSales.stroke}
+            fill={colors.totalSales.fill}
+            strokeWidth={2}
+            name='Total Sales'
+            unit='$'
           />
         </AreaChart>
       </ResponsiveContainer>
