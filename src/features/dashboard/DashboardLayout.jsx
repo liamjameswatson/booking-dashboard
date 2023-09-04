8c97eb1f1cd7798d76d97a62ac94e12682137a48
Liam <liamjwatson@aol.com>
2023-09-04 21:57:16 +0100

    menu now opens and closes when clicked, (on booking and cabin page)

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -21,7 +21,7 @@ const StyledDashboardLayout = styled.div`
 function DashboardLayout() {
   const { bookings, isLoading: isLoadingRecentBookings } = useRecentBooking();
 
-  const { stays, confirmedStays, isLoadingRecentStays, numDays } =
+  const { confirmedStays, isLoadingRecentStays, numDays } =
     useRecentStays();
 
   const { cabins, isLoading: isLoadingCabins } = useCabins();
